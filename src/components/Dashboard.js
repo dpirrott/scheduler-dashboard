a48e579322c401a23815b33a304e54b8093f20a9
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-31 14:43:30 -0700

    Add Panels component to dashboard, dynamically generate each panel from fake data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,54 @@ import React, { Component } from 'react';
 
 import classnames from 'classnames';
 import Loading from './Loading';
+import Panel from './Panel';
+
+const data = [
+  {
+    id: 1,
+    label: 'Total Interviews',
+    value: 6,
+  },
+  {
+    id: 2,
+    label: 'Least Popular Time Slot',
+    value: '1pm',
+  },
+  {
+    id: 3,
+    label: 'Most Popular Day',
+    value: 'Wednesday',
+  },
+  {
+    id: 4,
+    label: 'Interviews Per Day',
+    value: '2.3',
+  },
+];
 
 class Dashboard extends Component {
-  state = { loading: true };
+  state = { loading: false };
 
   render() {
     const dashboardClasses = classnames('dashboard');
+
+    const Panels = data.map((panel) => {
+      return (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      );
+    });
 
     return (
       <>
         {this.state.loading ? (
           <Loading />
         ) : (
-          <main className={dashboardClasses} />
+          <main className={dashboardClasses}>{Panels}</main>
         )}
       </>
     );
